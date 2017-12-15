26fd6bbbb98b366f1ef1f454b6aeeccde8d72fdd
Ben Deutsch <ben.deutschiii@gmail.com>
2017-12-15 01:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ function add(a, b) {
   return a + b;
 }
 add (5, 2);
+
 function subtract(a, b) {
   return a - b;
 }
 subtract (52 - 21);
+
 function multiply(a, b) {
   return a * b;
 }
 multiply (865 * 432);
+
+function divide(a, b) {
+  return a / b;
+}
+divide (432 / 51);
